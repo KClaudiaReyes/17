37d7e2f7a18f1bda70d5fe9e4395935610f06f53
clauR <creyesccf@gmail.com>
2026-03-04 22:56:17 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary < int , string> inventario = new Dictionary < int , string>();
+            int opcion = 0;
+
+            do
+            {
+
+
+
+
+            }while (opcion!=0);
+
+
+        }
+    }
+}
